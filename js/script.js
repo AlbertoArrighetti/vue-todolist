996f572dbb46b2ea0dcb30ae242ddc64df282658
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-20 15:40:32 +0100

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -4,6 +4,28 @@ createApp({
     data() {
         return {
             // data
+            toDoList: [
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: "Finire l'esercizio di oggi",
+                    done: false,
+                },
+                {
+                    text: "Pulire casa",
+                    done: true,
+                },
+                {
+                    text: "Comprare il pane",
+                    done: false,
+                },
+                {
+                    text: "Portare fuori il cane",
+                    done: true,
+                },
+            ]
         }
     },
     methods: {
